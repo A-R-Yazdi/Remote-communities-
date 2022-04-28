efb9ed1d74e820d14e6c53f0ef0a553b03a54fd4
Ling-Jun <zhoulingjunjeff@gmail.com>
2022-04-28 14:35:46 -0500

    Updated local package Preps, removed old NBs from Ali,

--- notebooks/config.py
+++
@@ -1,3 +0,0 @@
-EXPERIMENT = "indices"  # Name of experiment
-RUN_NAME = "initial assessment"  # Name of iteration (that will show
-ARTIFACT_DIR = "../artifacts/nbooks"  # Dir for notebook outputs where plots are saved
