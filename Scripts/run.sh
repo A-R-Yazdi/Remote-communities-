ec4a1c10854504c6d00b3c5fd8dd8f6bea4031f8
Ling-Jun <zhoulingjunjeff@gmail.com>
2022-06-17 15:39:20 -0500

    no message

---
+++ Scripts/run.sh
@@ -0,0 +1,4 @@
+pip install -r train/requirements.txt
+# the data path, model root path, figure root path all should change accordingly
+python train/train.py -d ../data/df2_processed.csv -mr train/ -fr train/
+
